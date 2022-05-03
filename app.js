57efe27b88e7a1f4f7c23a5aa1ac6b4ec910fd48
SammieStephenson <102987799+SammieStephenson@users.noreply.github.com>
2022-05-03 13:45:43 -0400

    Update app.js
    Update Sam Second

--- app.js
+++ app.js
@@ -25,14 +25,40 @@ function checkPrime(numArray) {
 
 checkPrime([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
+      
 
+// Medium 1
 let user = prompt("Type a Letter");
-function vowelChecker(){ 
-    if (user === a,e,i,o,u){ 
-        console.log("This is a vowel")
+function vowelChecker(user) {
+    if (user === "a" || "e" || "i" || "o" || "u") {
+        return console.log("This is a vowel");
     } else {
-        console.log("This is not a vowel");
+        return console.log("This is not a vowel");
     }
 };
-        
+
+vowelChecker(user);
+
+
+// Medium 3
+let num1 = prompt("Choose a 3 digit number");
+let num2 = prompt("Choose another 3 digit number");
+let y = num1;
+let x = num2;
+let quotient = Math.floor(y / x);
+let remainder = y % x;
+
+function bigNumber() {
+    if (remainder === 0) {
+        alert(`${quotient}`)
+    } else {
+        alert("Try Again")
+    }
+};
+
+bigNumber();
+
+
+// Hard 1
+f
     
